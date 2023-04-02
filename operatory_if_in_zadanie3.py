e4b47d9de730727c93b1ca0bb48f37bb676b61e6
Jakub Loniewski <87779513+Jakub-Loniewski@users.noreply.github.com>
2023-04-02 22:31:53 +0200

    Quest

---
+++ operatory_if_in_zadanie3.py
@@ -0,0 +1,11 @@
+
+value = None
+
+if value is True:
+    print("To prawda")
+elif value is False:
+    print("to fałsz")
+elif value is None:
+    print(" to none")
+else:
+    print("inna wartosc")
